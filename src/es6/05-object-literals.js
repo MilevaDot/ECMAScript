b108f4517e4bf73dc2e8c2cb80e1f37d1e6dbb94
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2022-12-10 17:25:05 -0500

    En EC6 se puede asignar directamente lo que se llame desde una función en la función.

---
+++ src/es6/05-object-literals.js
@@ -0,0 +1,12 @@
+// enhaced object literals
+
+function newUser(user, age, country, uId) {
+    return {
+        user: user,
+        age: age,
+        country: country,
+        id: iId,
+    }
+}
+
+console.log(newUser("Mileva.~", 25, "PE", 1));
