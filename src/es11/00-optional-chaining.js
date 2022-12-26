039a950a7cc4265ef75a315d4b5217fdc1f54e53
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2022-12-26 16:15:43 -0500

    Si un elemento del objeto no existe, podemos poner ? para indicar que puede o no estar y si no está devolverá undefined.

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,13 @@
+const users = {
+    aaron: {
+        country: 'PE'
+    },
+    ana: {
+        country: 'CO'
+    }
+}
+
+// console.log(users.aaron);
+// console.log(users.aaron.age);
+// console.log(users.bebeloper.country);
+console.log(users?.bebeloper?.country);
